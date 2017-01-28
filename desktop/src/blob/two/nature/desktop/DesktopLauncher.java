4af6dfc67a21f9886d38e064474f773c2962f3a7
Mark Weinreuter <mark@weinreuter.org>
2017-01-28 11:05:10 +0100

    Initial libgdx setup.

---
+++ desktop/src/blob/two/nature/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package blob.two.nature.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import blob.two.nature.NatureBlobGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NatureBlobGame(), config);
+	}
+}
