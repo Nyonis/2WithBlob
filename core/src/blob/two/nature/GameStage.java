c7c765db76a1e442a01d69485baa144c6da2fba5
Mark Weinreuter <mark@weinreuter.org>
2017-01-29 16:34:01 +0100

    count eat mes!

--- core/src/blob/two/nature/GameStage.java
+++ core/src/blob/two/nature/GameStage.java
@@ -11,16 +11,9 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.ContactImpulse;
-import com.badlogic.gdx.physics.box2d.ContactListener;
-import com.badlogic.gdx.physics.box2d.Manifold;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import helper.MapConvertHelper;
 
 import java.util.ArrayList;
 
@@ -36,6 +29,9 @@ public abstract class GameStage extends MyStage {
     public static final Integer ID_DIE = 2;
     public static final Integer ID_ARMPROJECTILE = 3;
     private final OrthographicCamera cameraP;
+    private final Number has;
+    private final Number max;
+    private final Group gCounter;
     public OrthographicCamera camera;
 
     private OrthogonalTiledMapRenderer renderer;
@@ -49,6 +45,7 @@ public abstract class GameStage extends MyStage {
     private TiledMapTileLayer foreGroundLayer;
     private ArrayList<Item> toDestroy = new ArrayList<Item>();
     private int collectibleCounter;
+    private int eatCounter;
 
 
     public GameStage(NatureBlobGame natureBlobGame, String mapName) {
@@ -72,32 +69,46 @@ public abstract class GameStage extends MyStage {
         //Init physics with -10 units gravity in the y-axis
         //MUST be called before loadMap()
         initPhysics(new Vector2(0f, -230.0f));
-        
+
         // always render a tile map
         renderer = new OrthogonalTiledMapRenderer(map);
         renderer.setView(camera);
-        
+
         b2dDebugRenderer = new Box2DDebugRenderer();
 
 
         PolygonShape shapeBlob = new PolygonShape();
         Vector2 textureDelta = new Vector2(64, 64);
-        Vector2[] vertices= new Vector2[7];
-        vertices[0] = new Vector2(104.f,101.f).sub(textureDelta).scl(WORLD_FAC);
-        vertices[1] = new Vector2(52.f,104.f).sub(textureDelta).scl(WORLD_FAC);
-        vertices[2] = new Vector2(-2.f,49.f).sub(textureDelta).scl(WORLD_FAC);
-        vertices[3] = new Vector2(11.f,29.f).sub(textureDelta).scl(WORLD_FAC);
-        vertices[4] = new Vector2(83.f,24.f).sub(textureDelta).scl(WORLD_FAC);
-        vertices[5] = new Vector2(106.f,34.f).sub(textureDelta).scl(WORLD_FAC);
-        vertices[6] = new Vector2(113.f,70.f).sub(textureDelta).scl(WORLD_FAC);
+        Vector2[] vertices = new Vector2[7];
+        vertices[0] = new Vector2(104.f, 101.f).sub(textureDelta).scl(WORLD_FAC);
+        vertices[1] = new Vector2(52.f, 104.f).sub(textureDelta).scl(WORLD_FAC);
+        vertices[2] = new Vector2(-2.f, 49.f).sub(textureDelta).scl(WORLD_FAC);
+        vertices[3] = new Vector2(11.f, 29.f).sub(textureDelta).scl(WORLD_FAC);
+        vertices[4] = new Vector2(83.f, 24.f).sub(textureDelta).scl(WORLD_FAC);
+        vertices[5] = new Vector2(106.f, 34.f).sub(textureDelta).scl(WORLD_FAC);
+        vertices[6] = new Vector2(113.f, 70.f).sub(textureDelta).scl(WORLD_FAC);
         shapeBlob.set(vertices);
         playerBlob = new PlayerBlob(natureBlobGame.blobAtlas, shapeBlob, b2dWorld);
         this.addActor(playerBlob);
 
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(128* WORLD_FAC, 20 * WORLD_FAC);
+        shape.setAsBox(128 * WORLD_FAC, 20 * WORLD_FAC);
         playerNature = new PlayerNature(shape, b2dWorld);
         this.addActor(playerNature);
+
+
+        gCounter = new Group();
+        has = new Number(0);
+        max = new Number(3);
+        Number slash = new Number(12);
+        max.change(5);
+        float size = 40;
+        slash.setPosition(size, 0);
+        max.setPosition(size * 2, 0);
+        gCounter.addActor(has);
+        gCounter.addActor(max);
+        gCounter.addActor(slash);
+        addActor(gCounter);
 
         loadMap(mapName);
 
@@ -105,14 +116,11 @@ public abstract class GameStage extends MyStage {
         renderer = new OrthogonalTiledMapRenderer(map);
         renderer.setView(camera);
 
-
-        //addActor(new Number(0));
-
         create();
     }
 
     @SuppressWarnings("static-access")
-	private void initPhysics(Vector2 gravity) {
+    private void initPhysics(Vector2 gravity) {
         b2dWorld = new World(gravity, true);
         b2dWorld.setVelocityThreshold(1000.f);
     }
@@ -149,17 +157,17 @@ public abstract class GameStage extends MyStage {
                     if (key.equals("Blob")) {
                         // set postion via physic!!
 
-                        playerBlob.b2dFigureBody.setTransform(x * WORLD_FAC, y* WORLD_FAC, 0);
-                        playerBlob.b2dArmProjectile.setTransform(x* WORLD_FAC, y* WORLD_FAC, 0);
+                        playerBlob.b2dFigureBody.setTransform(x * WORLD_FAC, y * WORLD_FAC, 0);
+                        playerBlob.b2dArmProjectile.setTransform(x * WORLD_FAC, y * WORLD_FAC, 0);
 
                     } else if (key.equals("Cloud")) {
-                        playerNature.hitbox.setTransform(x* WORLD_FAC, y* WORLD_FAC, 0);
-                    } else  if (key.equals("Collectible")) {
-                        collectibleCounter ++;
+                        playerNature.hitbox.setTransform(x * WORLD_FAC, y * WORLD_FAC, 0);
+                    } else if (key.equals("Collectible")) {
+                        collectibleCounter++;
                         CircleShape s = new CircleShape();
                         s.setRadius(32 * WORLD_FAC);
                         Item a = new Item(b2dWorld, s, new Texture("kugel.png"));
-                        a.setPos(x + ts/2, y+ ts/2);
+                        a.setPos(x + ts / 2, y + ts / 2);
                         addActor(a);
                     } else {
                         System.out.println("Unknown key: " + key);
@@ -167,6 +175,7 @@ public abstract class GameStage extends MyStage {
                 }
             }
         }
+        max.change(collectibleCounter);
         //map.getProperties().get("bubber", String.class);
         //MapLayers layers = map.getLayers();
         //TiledMapTileLayer l = (TiledMapTileLayer) layers.get(0);
@@ -182,9 +191,9 @@ public abstract class GameStage extends MyStage {
 
     public void doPhysicsStep() {
 
-    	//TODO Dynamisch an die Framerate anpassen
-    	playerBlob.doPhysics();
-    	b2dWorld.step(1/60f, 3, 1);
+        //TODO Dynamisch an die Framerate anpassen
+        playerBlob.doPhysics();
+        b2dWorld.step(1 / 60f, 3, 1);
     }
 
     @Override
@@ -197,8 +206,8 @@ public abstract class GameStage extends MyStage {
             }
             toDestroy.clear();
         }
-        
-        playerBlob.setPosition(playerBlob.b2dFigureBody.getPosition().x* WORLD_SCALE, playerBlob.b2dFigureBody.getPosition().y* WORLD_SCALE);
+
+        playerBlob.setPosition(playerBlob.b2dFigureBody.getPosition().x * WORLD_SCALE, playerBlob.b2dFigureBody.getPosition().y * WORLD_SCALE);
 
         Vector2 end = playerBlob.b2dArmProjectile.getPosition().cpy();
         Vector2 dir = playerBlob.b2dFigureBody.getPosition().cpy().sub(end);
@@ -210,19 +219,19 @@ public abstract class GameStage extends MyStage {
 
         // move cloud if it gets out of screen
         playerNature.hitbox.setTransform(
-        		Math.max(playerNature.hitbox.getPosition().x, camera.position.x - this.getViewport().getScreenWidth()/2*camera.zoom),
-        		Math.max(playerNature.hitbox.getPosition().y, camera.position.y - this.getViewport().getScreenHeight()/2*camera.zoom), 0f);
+                Math.max(playerNature.hitbox.getPosition().x, camera.position.x - this.getViewport().getScreenWidth() / 2 * camera.zoom),
+                Math.max(playerNature.hitbox.getPosition().y, camera.position.y - this.getViewport().getScreenHeight() / 2 * camera.zoom), 0f);
         playerNature.hitbox.setTransform(
-        		Math.min(playerNature.hitbox.getPosition().x, camera.position.x + this.getViewport().getScreenWidth()/2*camera.zoom),
-        		Math.min(playerNature.hitbox.getPosition().y, camera.position.y + this.getViewport().getScreenHeight()/2*camera.zoom), 0f);
-        
-        playerNature.setPosition(playerNature.hitbox.getPosition().x* WORLD_SCALE , playerNature.hitbox.getPosition().y* WORLD_SCALE);
+                Math.min(playerNature.hitbox.getPosition().x, camera.position.x + this.getViewport().getScreenWidth() / 2 * camera.zoom),
+                Math.min(playerNature.hitbox.getPosition().y, camera.position.y + this.getViewport().getScreenHeight() / 2 * camera.zoom), 0f);
+
+        playerNature.setPosition(playerNature.hitbox.getPosition().x * WORLD_SCALE, playerNature.hitbox.getPosition().y * WORLD_SCALE);
 
         boolean needsFlip = false;
-        if ((angle > 90 || angle < -90) && !playerBlob.figure.currentFrame.isFlipX()){
+        if ((angle > 90 || angle < -90) && !playerBlob.figure.currentFrame.isFlipX()) {
             needsFlip = true;
         }
-        if ((angle < 90 && angle > -90 ) && playerBlob.figure.currentFrame.isFlipX()){
+        if ((angle < 90 && angle > -90) && playerBlob.figure.currentFrame.isFlipX()) {
             needsFlip = true;
         }
 
@@ -232,14 +241,15 @@ public abstract class GameStage extends MyStage {
         Vector3 oldCameraPosition = camera.position.cpy();
         camera.position.set(playerBlob.getX(), playerBlob.getY(), 0);
         Vector3 cameraDelta = camera.position.cpy().sub(oldCameraPosition);
-        playerBlob.addDeltaToArmTarget(new Vector2(cameraDelta.x, cameraDelta.y).scl(1 / camera.zoom* WORLD_FAC));
+        playerBlob.addDeltaToArmTarget(new Vector2(cameraDelta.x, cameraDelta.y).scl(1 / camera.zoom * WORLD_FAC));
 
         camera.update();
-        cameraP.position.set(camera.position.x * WORLD_FAC, camera.position.y * WORLD_FAC,0);
+        cameraP.position.set(camera.position.x * WORLD_FAC, camera.position.y * WORLD_FAC, 0);
         cameraP.update();
         renderer.setView(camera);
         renderer.render(preLayers);
         b2dDebugRenderer.render(b2dWorld, cameraP.combined);
+        gCounter.setPosition(camera.position.x - w / 2 * camera.zoom + 10, camera.position.y + h / 2 * camera.zoom - 60);
 
         // make the scene draw stuff
         super.draw();
@@ -254,23 +264,35 @@ public abstract class GameStage extends MyStage {
                 Object a = contact.getFixtureA().getBody().getUserData();
                 Object b = contact.getFixtureB().getBody().getUserData();
                 if (a != null && b != null) {
-                    if (isItem(a, b))
+                    if (isItem(a, b)) {
                         toDestroy.add(((Item) a));
-                    else if (isItem(b, a))
+                        eat();
+                    } else if (isItem(b, a)) {
                         toDestroy.add(((Item) b));
-                    else if (isDie(a, b))
+                        eat();
+                    } else if (isDie(a, b))
                         die();
                 } else if (a != null || b != null) {
-                	if (isArmCollision(a, b))
-                		playerBlob.lock();
+                    if (isArmCollision(a, b))
+                        playerBlob.lock();
                     else if (isArmCollision(b, a))
-                    	playerBlob.lock();
+                        playerBlob.lock();
+                }
+            }
+
+            private void eat() {
+
+                eatCounter++;
+                has.change(eatCounter);
+                if (eatCounter == collectibleCounter){
+                    System.out.println("Won!!!");
                 }
             }
 
 
             @Override // TODO
-            public void endContact(Contact contact) {}
+            public void endContact(Contact contact) {
+            }
 
             @Override
             public void postSolve(Contact arg0, ContactImpulse arg1) {
@@ -294,12 +316,12 @@ public abstract class GameStage extends MyStage {
     }
 
     private boolean isItem(Object a, Object b) {
-        return  (b.equals(ID_PLAYER) && a instanceof Item);
+        return (b.equals(ID_PLAYER) && a instanceof Item);
 
     }
-    
+
     private boolean isArmCollision(Object a, Object b) {
-    	return (a != null && a.equals(ID_ARMPROJECTILE));
+        return (a != null && a.equals(ID_ARMPROJECTILE));
     }
 
 
